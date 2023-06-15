62fd072fcc52dab79cfbaeaf56e104953fddfe08
hikaru1224 <130636591+tuna2142@users.noreply.github.com>
2023-06-15 15:26:27 +0900

    演習13-2

--- v0615/Form1.Designer.cs
+++ v0615/Form1.Designer.cs
@@ -28,12 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(216, 207);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(336, 207);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(449, 207);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 2;
+            label3.Text = "label3";
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- v0615/Form1.cs
+++ v0615/Form1.cs
@@ -2,9 +2,80 @@ namespace v0615
 {
     public partial class Form1 : Form
     {
+        static Random rand = new Random();
+        int vx1 = rand.Next(-10, 11);
+        int vy1 = rand.Next(-10, 11);
+        int vx2 = rand.Next(-10, 11);
+        int vy2 = rand.Next(-10, 11);
+        int vx3 = rand.Next(-10, 11);
+        int vy3 = rand.Next(-10, 11);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx1;
+            label1.Top += vy1;
+
+            label2.Left += vx2;
+            label2.Top += vy2;
+
+            label3.Left += vx3;
+            label3.Top += vy3;
+
+            if (label1.Left <= 0)
+            {
+                vx1 = Math.Abs(vx1 + (vx1 / 10));
+            }
+            else if (label1.Right >= ClientSize.Width)
+            {
+                vx1 = -Math.Abs(vx1 + (vx1 / 10));
+            }
+            if (label1.Top <= 0)
+            {
+                vy1 = Math.Abs(vy1 + (vy1 / 10));
+            }
+            else if (label1.Bottom >= ClientSize.Height)
+            {
+                vy1 = -Math.Abs(vy1 + (vy1 / 10));
+            }
+
+            if (label2.Left <= 0)
+            {
+                vx2 = Math.Abs(vx2 + (vx2 / 10));
+            }
+            else if (label2.Right >= ClientSize.Width)
+            {
+                vx2 = -Math.Abs(vx2 + (vx2 / 10));
+            }
+            if (label2.Top <= 0)
+            {
+                vy2 = Math.Abs(vy2 + (vy2 / 10));
+            }
+            else if (label2.Bottom >= ClientSize.Height)
+            {
+                vy2 = -Math.Abs(vy2 + (vy2 / 10));
+            }
+
+            if (label3.Left <= 0)
+            {
+                vx3 = Math.Abs(vx3 + (vx3 / 10));
+            }
+            else if (label3.Right >= ClientSize.Width)
+            {
+                vx3 = -Math.Abs(vx3 + (vx3 / 10));
+            }
+            if (label3.Top <= 0)
+            {
+                vy3 = Math.Abs(vy3 + (vy3 / 10));
+            }
+            else if (label3.Bottom >= ClientSize.Height)
+            {
+                vy3 = -Math.Abs(vy3 + (vy3 / 10));
+            }
         }
     }
 }
